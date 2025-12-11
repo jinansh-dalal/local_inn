e678e8cd7dcec0ee607a020a7d5f39d57d6f414c
Jinansh Dalal <jinanshdalal.24@kgpian.iitkgp.ac.in>
2025-12-11 16:11:03 +0530

    Made Setup Script

--- README.md
+++ README.md
@@ -1,13 +1,5 @@
 # local_inn
 
 ```
-python -m venv local-inn
-source local-inn/bin/activate
-pip install torch
-pip install numpy
-pip install FrEIA
-pip install tqdm
-pip install tensorboard
-pip install PyYAML
-pip install rosbags
+source setup.bash
 ```
---
+++ setup.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+VENV_DIR="local-inn"
+
+if [ ! -d "$VENV_DIR" ]; then
+    python3 -m venv "$VENV_DIR"
+fi
+
+source $VENV_DIR/bin/activate
+
+REQ_FILE="requirements.txt"
+
+if [ -f "$REQ_FILE" ]; then
+    pip install --upgrade pip
+    pip install -r "$REQ_FILE"
+else
+    echo "Warning: $REQ_FILE not found."
+fi
+
+source $VENV_DIR/bin/activate
